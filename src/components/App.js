42d72e15f4d2aebe44349eeedd1cc4d4a31e84c8
Apogee <saiganeshs902@gmail.com>
2024-11-24 02:11:04 +0530

    download_ele

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,45 @@ function App() {
   const [js, setJs] = useLocalStorage('js', '')
   const [srcDoc, setSrcDoc] = useState('')
 
+  // Download files with content as srcdoc in a single html file
+  const downloadFiles = () => {
+    const element = document.createElement('a');
+    const file = new Blob([srcDoc], {type: 'text/html'});
+    element.href = URL.createObjectURL(file);
+    element.download = "index.html";
+    document.body.appendChild(element); //firefox requires this
+    element.click();
+    document.body.removeChild(element);
+  }
+
+  // Download files with content as separate files
+  const downloadSeparateFiles = () => {
+    const elementHtml = document.createElement('a');
+    const fileHtml = new Blob([html], {type: 'text/html'});
+    elementHtml.href = URL.createObjectURL(fileHtml);
+    elementHtml.download = "index.html";
+    document.body.appendChild(elementHtml); 
+    elementHtml.click();
+
+    const elementCss = document.createElement('a');
+    const fileCss = new Blob([css], {type: 'text/css'});
+    elementCss.href = URL.createObjectURL(fileCss);
+    elementCss.download = "style.css";
+    document.body.appendChild(elementCss); 
+    elementCss.click();
+
+    const elementJs = document.createElement('a');
+    const fileJs = new Blob([js], {type: 'text/javascript'});
+    elementJs.href = URL.createObjectURL(fileJs);
+    elementJs.download = "script.js";
+    document.body.appendChild(elementJs);
+    elementJs.click();
+
+    document.body.removeChild(elementHtml);
+    document.body.removeChild(elementCss);
+    document.body.removeChild(elementJs);
+  }
+
   useEffect(() => {
     const timeout = setTimeout(() => { // here, both css, js file called into html, this will be passes as srcdoc
       setSrcDoc(`
@@ -23,7 +62,7 @@ function App() {
   }, [html, css, js])
 
   return (
-    <>
+    <div className='block'>
       <div className="pane top-pane">
         <Editor
           language="xml"
@@ -32,21 +71,19 @@ function App() {
           onChange={setHtml}
 
         />
-        <button onClick={() => {navigator.clipboard.writeText(html)}} >Copy</button>
         <Editor
           language="css"
           displayName="CSS"
           value={css}
           onChange={setCss}
         />
-        <button onClick={() => {navigator.clipboard.writeText(css)}} >Copy</button>
         <Editor
           language="javascript"
           displayName="JS"
           value={js}
           onChange={setJs}
         />
-        <button onClick={() => {navigator.clipboard.writeText(js)}} >Copy</button>
+
       </div>
       <div className="pane">
         <iframe
@@ -58,7 +95,9 @@ function App() {
           height="100%"
         />
       </div>
-    </>
+      <button className="btn" onClick={downloadFiles}>Download All</button>
+      <button className="btn" onClick={downloadSeparateFiles}>Download Files</button>
+    </div>
   )
 }
 
